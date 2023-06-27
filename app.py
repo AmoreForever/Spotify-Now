899fba88c6ce8f826da71b94fc21e6d3a3ee28b1
Amore <me.thefarkhodov@gmail.com>
2023-06-27 12:58:11 +0500

    Update

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template
 import spotipy
+
 from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
 
 
-client_id = "e0708753ab60499c89ce263de9b4f57a"
-client_secret = "80c927166c664ee98a43a2c0e2981b4a"
-redirect_uri = "https://fuccsoc.com/"
+client_id = "dc60d69ab3294f138668d4018bbc4021"
+client_secret = "03292c8175c746b28348c668b215e717"
+redirect_uri = "http://spotitoken.farkhodovme.tk/"
 
 scope = (
     "user-read-playback-state playlist-read-private playlist-read-collaborative"
-    " app-remote-control user-modify-playback-state user-library-modify"
-    " user-library-read"
+    "app-remote-control user-modify-playback-state user-library-modify"
+    "user-library-read"
 )
 auth_manager = SpotifyOAuth(
     client_id=client_id,
@@ -21,7 +22,23 @@ auth_manager = SpotifyOAuth(
     scope=scope,
 )
 
-sp = spotipy.Spotify(auth_manager=auth_manager)
+try:
+    with open(".cache") as f:
+        t = f.read()
+        if t == "None":
+            print("You can get token from https://spotitoken.farkhodovme.tk/")
+            type_token = input("Type token: ")
+            with open(".cache", "w") as f:
+                f.write(type_token)
+        else:
+            token = t
+except FileNotFoundError:
+    print("You can get token from https://spotitoken.farkhodovme.tk/")
+    type_token = input("Type token: ")
+    with open(".cache", "w") as f:
+        f.write(type_token)
+
+sp = spotipy.Spotify(auth=open(".cache").read(), auth_manager=auth_manager)
 
 @app.route("/")
 def now_playing():
@@ -36,7 +53,6 @@ def now_playing():
     duration_ms = current_track["item"]["duration_ms"]
     release_date = current_track["item"]["album"]["release_date"]
     image_url = current_track["item"]["album"]["images"][0]["url"]
-
     return render_template(
         "index.html",
         track_name=track_name,
